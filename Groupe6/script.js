27b80ff2eefc56919d4c4ca1a3607ccfba3c73f9
Camille Dieppois <camille.dieppois@viacesi.fr>
2024-11-12 13:46:41 +0100

    s

--- Groupe6/script.js
+++ Groupe6/script.js
@@ -3,3 +3,9 @@ let p1 = document.getElementById("p1");
 p1.addEventListener('mouseover', (e) => {
   console.log("Bonjour");
 });
+
+let camille = document.getElementById("camille");
+
+camille.addEventListener('mouseover', (e) =>{
+  console.log("bonjour");
+});
