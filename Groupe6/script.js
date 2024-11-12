60870b17e1990b7b1fc70920aba7b51b31b5e3cf
Lucas SAILLOT <lucas.saillot@viacesi.fr>
2024-11-12 13:43:54 +0100

    test

--- Groupe6/script.js
+++ Groupe6/script.js
@@ -1,16 +1,8 @@
 let p1 = document.getElementById("p1");
 
 p1.addEventListener('mouseover', (e) => {});
-=======
 let p1 = document.getElementById("p1");
 
 p1.addEventListener('mouseover', (e) => {
   console.log("Bonjour");
 });
-
-
-let lucas = document.getElementById("lucas");
-
-lucas.addEventListener('mouseover', (e) => (
-    console.log("Bonjour", lucas)
-));
