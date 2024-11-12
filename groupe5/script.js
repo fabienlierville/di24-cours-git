76814f65c53604532a08247301dcd053d5919090
Louise Ducrocq <louise.ducroc@gmail.com>
2024-11-12 13:53:14 +0100

    fefdede

--- groupe5/script.js
+++ groupe5/script.js
@@ -1,17 +1,17 @@
 let pl = document.getElementById("pl")
 
-pl.addEventListener('mouseover', (0) => {
+pl.addEventListener('mouseover', (e) => {
     console.log("Bonjour")
 })
 
 let la = document.getElementById("la")
 
-la.addEventListener('mouseover', (0) => {
+la.addEventListener('mouseover', (e) => {
     console.log("Bonjour")
 })
 
 let li = document.getElementById("li")
 
-li.addEventListener('mouseover', (0) => {
+li.addEventListener('mouseover', (e) => {
     console.log("Bonjour")
 })
