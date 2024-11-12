15c30e8815c0f2a8431fbe9b93c9f01b25c583dd
Bastien <bastien.grenot@viacesi.fr>
2024-11-12 15:37:27 +0100

    a

--- Groupe2/script.js
+++ Groupe2/script.js
@@ -3,16 +3,25 @@ let contenu = p1.textContent;
 
 p1.addEventListener('mouseover', (e) => {
     console.log("Bonjour : " + contenu)
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    document.body.style.backgroundColor = randomColor; // Appliquer la couleur au fond de la page entière
+    console.log("Couleur de fond changée :", randomColor);
 })
 
 let p2 = document.getElementById("p2");
 let contenu2 = p2.textContent;
 p2.addEventListener('mouseover', (e) => {
     console.log("Bonjour : " + contenu2)
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    document.body.style.backgroundColor = randomColor; // Appliquer la couleur au fond de la page entière
+    console.log("Couleur de fond changée :", randomColor);
 })
 
 let p3 = document.getElementById("p3");
 let contenu3 = p3.textContent;
 p3.addEventListener('mouseover', (e) => {
     console.log("Bonjour : " + contenu3)
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    document.body.style.backgroundColor = randomColor; // Appliquer la couleur au fond de la page entière
+    console.log("Couleur de fond changée :", randomColor);
 })
