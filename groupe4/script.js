c495c1dc3c330a2f8db8fd5098be674523cafe88
Maëlia CHENAVAZ <maelia.chenavaz@viacesi.fr>
2024-11-12 13:45:42 +0100

    test

---
+++ groupe4/script.js
@@ -0,0 +1,9 @@
+let p3 = document.getElementById("p1"); 
+
+p3.addEventListener('mouseover', (e) => {
+    console.log("Jessica")
+})
+let p2 = document.getElementById ("P1");
+p1.addEventListener ('mouseover', (e) => {
+    console.log ("AL");
+});
