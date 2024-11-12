89860be237478c17f7872480099079c804d75f66
Toitoine <antoinelimare76700@gmail.com>
2024-11-12 13:52:43 +0100

    ddsdfzdzdszd

--- Groupe2/script.js
+++ Groupe2/script.js
@@ -12,7 +12,6 @@ p2.addEventListener('mouseover', (e) => {
 })
 
 let p3 = document.getElementById("p3");
-let contenu2 = p2.textContent;
 p3.addEventListener('mouseover', (e) =>{
     console.log('Bonjour');
 })
