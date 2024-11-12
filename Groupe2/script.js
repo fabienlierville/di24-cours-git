ad6ca7c86d5208b7ef8b395a912ab1c315863ffa
Toitoine <antoinelimare76700@gmail.com>
2024-11-12 13:40:01 +0100

    ss

--- Groupe2/script.js
+++ Groupe2/script.js
@@ -3,3 +3,9 @@ let p1 = document.getElementById("p1");
 p1.addEventListener('mouseover', (e) => {
     console.log("Bonjour : " + p1)
 })
+
+let p2 = document.getElementById("p2");
+
+p2.addEventListener('mouseover', (e) => {
+    console.log("Bonjour : " + p2)
+})
