d62f9b37f342b77752e248a99d196da81b425004
Jessica RIBEIRO <ribeirofj@yahoo.com>
2024-11-12 14:30:28 +0100

    gg

--- groupe4/script.js
+++ groupe4/script.js
@@ -1,8 +1,7 @@
 let p3 = document.getElementById("p1"); 
-
 p3.addEventListener('mouseover', (e) => {
     console.log("Jessica")
-})
+});
 let p2 = document.getElementById ("p2");
 p2.addEventListener ('mouseover', (e) => {
     console.log ("AL");
