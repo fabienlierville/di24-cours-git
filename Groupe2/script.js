1872c9b6deae823073d833ccf26d269453eb6dc4
Bastien <bastien.grenot@viacesi.fr>
2024-11-12 13:44:06 +0100

    FINI

--- Groupe2/script.js
+++ Groupe2/script.js
@@ -1,5 +1,6 @@
 let p1 = document.getElementById("p1");
+let contenu = p1.textContent;
 
 p1.addEventListener('mouseover', (e) => {
-    console.log("Bonjour : ")
+    console.log("Bonjour : " + contenu)
 })
